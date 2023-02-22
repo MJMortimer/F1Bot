1fe846c28c034e14e4b2da4256536352bc9bf6ff
MJMortimer <matthew.mortimer1@gmail.com>
2023-02-22 15:43:50 +1300

    The constructor standings command now creates and returns a canvas as a png embedded in the interaction reply

--- src/bot.ts
+++ src/bot.ts
@@ -44,8 +44,16 @@ client.on('interactionCreate', async (interaction: Interaction) => {
         }
 
         if(commandInteraction.commandName === F1ScheduleBotCommand.CONSTRUCTOR_STANDINGS){
-            const constructorStandingsTable = await getConstructorStandingsTable();
-            await interaction.editReply({content: constructorStandingsTable});
+            const canvas = await getConstructorStandingsCanvas();
+
+            if(!canvas){
+                await interaction.editReply("Something went wrong");
+                return;
+            }
+
+            const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'constructorstandings.png' });
+
+            await interaction.editReply({files: [attachment]});
             return;
         }
 
@@ -120,9 +128,17 @@ client.on('interactionCreate', async (interaction: Interaction) => {
         }
 
         if(commandInteraction.commandName === F1ScheduleBotCommand.TEST){
-            
+            const canvas = await getConstructorStandingsCanvas();
 
-            
+            if(!canvas){
+                await interaction.editReply("Something went wrong");
+                return;
+            }
+
+            const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'test.png' });
+
+            await interaction.editReply({files: [attachment]});
+            return;            
         }
 
         await interaction.editReply("Something went wrong");
@@ -223,6 +239,30 @@ const getSchedule = async (year: string, round: string) => {
     }
 }
 
+const getConstructorStandings = async () => {
+    const constructorStandings = await api.getCurrentSeasonConstructorStandings();
+
+    if(!constructorStandings){
+        return null;
+    }
+
+    const standings = [];
+
+    for (let i = 0; i < constructorStandings.length; i++) {
+        const constructorStanding = constructorStandings[i];
+
+        standings.push({
+            points: constructorStanding.points,
+            constructor: {
+                id: constructorStanding.Constructor.constructorId,
+                name: constructorStanding.Constructor.name
+            }
+        });
+    }
+
+    return standings;
+}
+
 const getDriverStandingsTable = async () => {
     const driverStandings = await api.getCurrentSeasonDriverStandings();
 
@@ -231,25 +271,6 @@ const getDriverStandingsTable = async () => {
     for (let i = 0; i < driverStandings.length; i++) {
         const driverStanding = driverStandings[i];
         tableEntries.push([driverStanding.position, driverStanding.points, `${driverStanding.Driver.givenName} ${driverStanding.Driver.familyName}`]);
-    }
-
-    const tableData = table(tableEntries, {align:['r', 'r', 'l']});
-
-    return (
-`\`\`\`
-${tableData}
-\`\`\``
-    );
-}
-
-const getConstructorStandingsTable = async () => {
-    const constructorStandings = await api.getCurrentSeasonConstructorStandings();
-
-    const tableEntries = [["Pos", "Pts", "Constructor"]];
-
-    for (let i = 0; i < constructorStandings.length; i++) {
-        const constructorStanding = constructorStandings[i];
-        tableEntries.push([constructorStanding.position, constructorStanding.points, constructorStanding.Constructor.name]);
     }
 
     const tableData = table(tableEntries, {align:['r', 'r', 'l']});
@@ -390,6 +411,86 @@ const getScheduleCanvas = async (year: string, round: string) => {
         if(isNextSession) {
             hasDrawnNextSession = true;
         }
+    }
+
+    return canvas;
+}
+
+const getConstructorStandingsCanvas = async () => {
+    const data = await getConstructorStandings();
+
+    if(!data){
+        return null;
+    }
+
+    const buffer = 20;
+    const titleSize = 50;
+    const titleSeparator = 20;
+    const headingSize = 40;
+    const headingSeparator = 15;
+    const entrantSize = 30;
+    const entrantSeparator = 10;
+
+    const canvasHeight = 
+        buffer +
+        titleSize +
+        titleSeparator +
+        headingSize +
+        headingSeparator +
+        (data.length * entrantSize) +
+        (data.length * entrantSeparator) +
+        buffer;
+
+    const canvas = Canvas.createCanvas(1000, canvasHeight);
+    const context = canvas.getContext('2d');
+    
+    const background = await Canvas.loadImage("build/images/bg.png");
+    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    const logo = await Canvas.loadImage("build/images/F1-logo.png");
+    context.drawImage(logo, (canvas.width - logo.width - buffer), 0); // There's already a bit of buffer built into the png, ignore it for the y value
+
+    // Draw the title
+    context.fillStyle = '#000000';
+    context.font = `${titleSize}px f1-bold`;
+    context.fillText("Constructor standings", 20, buffer + titleSize);
+
+    const headingY = buffer + titleSize + titleSeparator + headingSize;
+
+    // Draw the headings
+    context.font = `${headingSize}px f1-bold`;
+
+    context.textAlign = "right";
+    context.fillText("Pos", 180, headingY);
+
+    context.textAlign = "left";
+    context.fillText("Constructor", 240, headingY);
+
+    context.textAlign = "right";
+    context.fillText("Pts", 755, headingY);
+    context.fillText("Gap", 895, headingY);
+
+    
+
+    // Draw the standings
+    context.fillStyle = '#FFFFFF';
+    context.font = `${headingSize}px f1-reg`;
+    
+    for (let i = 0; i < data.length; i++) {
+        const standing = data[i];
+        const pointGap = i === 0 ? "- - -" : `${data[i - 1].points - data[i].points}`;
+
+        const entrantY = buffer + titleSize + titleSeparator + headingSize + headingSeparator + ((i + 1) * entrantSize) + (i * entrantSeparator);
+
+        context.textAlign = "right";
+        context.fillText(`${i + 1}`, 180, entrantY);
+
+        context.textAlign = "left";
+        context.fillText(standing.constructor.name, 240, entrantY, 375);
+
+        context.textAlign = "right";
+        context.fillText(standing.points, 755, entrantY);
+        context.fillText(pointGap, 895, entrantY);
     }
 
     return canvas;
